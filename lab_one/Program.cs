c36372545af236b44419ee8be9f48d086ff0a148
cifulton <cfulton@dunwoody.edu>
2020-01-22 14:34:11 -0600

    changing console to output

--- lab_one/Program.cs
+++ lab_one/Program.cs
@@ -6,7 +6,7 @@ namespace lab_one
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Chris");
         }
     }
 }
