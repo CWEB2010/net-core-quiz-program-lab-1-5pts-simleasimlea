2928cfc73edb1869f48fbbdcde15a275c048b073
Simonson <simlea@dunwoody.edu>
2020-01-24 17:40:24 -0600

    initial commit pre-code exercise

--- lab_one/Program.cs
+++ lab_one/Program.cs
@@ -4,6 +4,7 @@ namespace lab_one
 {
     class Program
     {
+		//pre add-code stage per Chris's pseudocode.
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Chris");
